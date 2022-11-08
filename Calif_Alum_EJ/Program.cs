c5d265b12c366890ac9bdfbe7575e24752ef7ac3
AlejandroJuarezGarcia <115949195+AlejandroJuarezGarcia@users.noreply.github.com>
2022-11-08 11:08:34 -0600

    Add files via upload

---
+++ Calif_Alum_EJ/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calif_Alum_EJ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre, apellido, grado, seccion, materia;
+            byte calif;
+            Console.WriteLine("Escriba el nombre del alumno");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Escriba el apellido del alumno");
+            apellido = Console.ReadLine();
+            Console.WriteLine("Escriba el grado del alumno");
+            grado = Console.ReadLine();
+            Console.WriteLine("Escriba la seccion del alumno");
+            seccion = Console.ReadLine();
+            Console.WriteLine("Escriba la materia a calificar");
+            materia = Console.ReadLine();
+            Console.WriteLine("Escriba la calificacion del alumno");
+            calif= Convert.ToByte (Console.ReadLine());
+            if (calif>=70)
+            {
+                Console.WriteLine("Nombre del alumno:{0}{1} \n Grupo y seccion:{2}{3} \n Materia:{4}"  , nombre, apellido, grado, seccion, materia);
+                Console.WriteLine("El alumno esta acreditado");
+                
+            }
+            else
+            {
+                Console.WriteLine("El alumno no esta acreditado");
+            }
+            Console.ReadKey();
+        }
+    }
+}
